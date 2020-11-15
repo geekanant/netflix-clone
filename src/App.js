42f2c5039ecc112fe09bc2d61f0ad71ce73809c5
geekanant <anantpatni1@gmail.com>
2020-11-15 22:17:51 +0530

    Setup fetching data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix Clone</h1>
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}>
+        TItle
+      </Row>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}>
+        TItle
+      </Row>
     </div>
   );
 }
